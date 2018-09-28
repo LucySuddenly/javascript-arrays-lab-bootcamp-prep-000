4dd1270f87e5a4e28e06159950d692ed45ecc5ff
Lucy Suddenly <suddenlylucy@gmail.com>
2018-09-28 19:38:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Add your functions and code here
-var kittens = ["Milo", "Otis", "Garfield"]
-
-function destructivelyAppendKitten(name)
+function destructivelyAppendKitten(name) {
+  kittens.unshift(name)
+}
